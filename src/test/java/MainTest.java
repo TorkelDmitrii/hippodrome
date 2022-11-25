9f68002c696f6b37942d48ae16d5882a0913b04d
Dmitrii <torkeldmitrii@outlook.com>
2022-11-25 14:59:09 +0300

    MainTest method's has been created

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 public class MainTest {
+
+    @Disabled
+    @Test
+    @Timeout(22)
+    public void mainTest() throws Exception {
+        Main.main(null);
+    }
 }
